2537517f4c867e63b4c264484e95362b5c4b6a75
LongVu3101 <vvl310104@gmail.com>
2024-04-27 14:35:09 +0700

    lab03

---
+++ lab03/hust/soict/dsai/aims/Aims/Aims.java
@@ -0,0 +1,7 @@
+package lab03.hust.soict.dsai.aims.Aims;
+
+public class Aims {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+	}
+}
---
+++ lab03/hust/soict/dsai/aims/Media/Playable.java
@@ -0,0 +1,5 @@
+package lab03.hust.soict.dsai.aims.Media;
+
+public interface Playable {
+	void play();
+}
