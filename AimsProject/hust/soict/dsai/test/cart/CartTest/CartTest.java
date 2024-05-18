24f0d606445b52d5b4403f69a3ab1281899ebda2
LongVu3101 <160836745+LongVu3101@users.noreply.github.com>
2024-05-18 11:30:21 +0700

    lab04

---
+++ AimsProject/hust/soict/dsai/test/cart/CartTest/CartTest.java
@@ -0,0 +1,30 @@
+package AimsProject.hust.soict.dsai.test.cart.CartTest;
+
+import AimsProject.hust.soict.dsai.aims.cart.Cart;
+import AimsProject.hust.soict.dsai.aims.media.DigitalVideoDisc;
+
+public class CartTest {
+	public static void main(String[] args ) {
+		//Create a new cart
+		Cart cart = new Cart();
+
+		//Create new dvd objects and add them to the cart
+
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
+		"Animation", "Roger ALtLers", 87, 19.95f);
+
+		cart.addMedia( dvd1) ;
+
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars",
+		"Science Fiction", "George Lucas", 87, 24.95f);
+		cart.addMedia(dvd2) ;
+
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Atadin",
+		"Animation", 18.99f) ;
+		cart.addMedia(dvd3) ;
+
+		//Test the print method
+		System.out.println(cart);
+		System.out.println("Total Cost is: "+cart.totalCost()); 
+	}
+}
