a9e378b7667cb103503f7f67903f46d6be0b279d
LongVu3101 <160836745+LongVu3101@users.noreply.github.com>
2024-06-02 23:26:54 +0700

    lab05

---
+++ AimsProject/src/hust/soict/dsai/aims/Aims/Aims.java
@@ -0,0 +1,7 @@
+package hust.soict.dsai.aims.Aims;
+
+public class Aims {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+	}
+}
---
+++ AimsProject/src/hust/soict/dsai/aims/Media/Disc.java
@@ -0,0 +1,31 @@
+package hust.soict.dsai.aims.Media;
+
+public class Disc extends Media {
+    private int length;
+    private String director;
+
+    public Disc(String title, String category, float cost, int length, String director) {
+        super(title, category, cost);
+        this.length = length;
+        this.director = director;
+    }
+    public Disc(String title) {
+		super(title);
+
+	}
+	public Disc(String title, String category, float cost) {
+		super(title, category, cost);
+
+	}
+	public Disc(String title, String category, String director, float cost) {
+		super(title, category, cost);
+		this.director = director;
+	}
+    public int getLength() {
+        return length;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+}
